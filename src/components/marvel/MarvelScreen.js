9561b86963fe955df95a840112c5fdd19306063e
Braian Leonel Vogrič <53317134+Vogric@users.noreply.github.com>
2020-11-06 14:17:11 -0300

    Hero list and selectors
    * HeroList file was created
    * Selectors folder was created
    * Two new selectors: getHeroById and getHeroesByPublisher

--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { HeroList } from "../heroes/HeroList";
 
 export const MarvelScreen = () => {
   return (
-    <div>
-      <h1>MarvelScreen WIP</h1>
+    <div className="container mt-4">
+      <h1>MarvelScreen</h1>
+      <hr />
+      <HeroList publisher="Marvel Comics" />
     </div>
   );
 };
