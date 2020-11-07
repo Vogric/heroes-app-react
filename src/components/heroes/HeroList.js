44f48c60d7da435d7b5de05186fcbe147a580dfd
Braian Leonel Vogrič <53317134+Vogric@users.noreply.github.com>
2020-11-07 09:13:56 -0300

    Arguments by URL
    * Valid argument validation
    * Extraction of arguments coming from url

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher";
 import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
+
+  if (!publisher) {
+    return <Redirect to="/" />;
+  }
 
   return (
     <div className="card-columns">
--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,24 @@
 import React from "react";
+import { Redirect, useParams } from "react-router-dom";
+import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
+  const { heroId } = useParams();
+  const hero = getHeroById(heroId);
+  console.log(hero);
+
+  if (!heroId) {
+    return <Redirect to="/" />;
+  }
+
+  const {
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+  } = hero;
+
   return (
     <div>
       <h1>HeroScreen WIP</h1>
