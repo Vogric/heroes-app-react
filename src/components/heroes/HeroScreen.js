dd08626c37f87032216ab39f5371a37c0de5520c
Braian Leonel Vogrič <53317134+Vogric@users.noreply.github.com>
2020-11-07 09:50:35 -0300

    HeroScreen component style
    * Back button
    * History validation

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Redirect, useParams } from "react-router-dom";
 import { getHeroById } from "../../selectors/getHeroById";
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
   const { heroId } = useParams();
   const hero = getHeroById(heroId);
   console.log(hero);
@@ -10,6 +10,10 @@ export const HeroScreen = () => {
   if (!heroId) {
     return <Redirect to="/" />;
   }
+
+  const handleBack = () => {
+    return history.length <= 2 ? history.push("/") : history.goBack();
+  };
 
   const {
     superhero,
@@ -20,8 +24,37 @@ export const HeroScreen = () => {
   } = hero;
 
   return (
-    <div>
-      <h1>HeroScreen WIP</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`../assets/heroes/${heroId}.jpg`}
+          alt={superhero}
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h3> {superhero} </h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            {" "}
+            <b>Publisher: </b> {publisher}{" "}
+          </li>
+          <li className="list-group-item">
+            {" "}
+            <b>Alter ego: </b> {alter_ego}{" "}
+          </li>
+          <li className="list-group-item">
+            {" "}
+            <b>First Appearance: </b> {first_appearance}{" "}
+          </li>
+        </ul>
+        <h5>Characters</h5>
+        <p> {characters} </p>
+
+        <button className="btn btn-outline-info" onClick={handleBack}>
+          Back
+        </button>
+      </div>
     </div>
   );
 };
