e9715afe3002e8a2cb27ea9ea6fc586941bb4dc6
Vogric <53317134+Vogric@users.noreply.github.com>
2020-11-24 14:53:28 -0300

    Logout with  useHistory

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,10 +1,23 @@
 import React, { useContext } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useHistory } from "react-router-dom";
 import { AuthContext } from "../../auth/AuthContext";
+import { types } from "../../types/types";
+
 export const Navbar = () => {
   const {
     user: { name },
+    dispatch,
   } = useContext(AuthContext);
+
+  const history = useHistory();
+
+  const handleLogout = () => {
+    dispatch({
+      type: types.logout,
+    });
+
+    history.replace("/login");
+  };
 
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
@@ -47,15 +60,9 @@ export const Navbar = () => {
           <span activeClassName="active" className="nav-item nav-link" exact>
             {name}
           </span>
-
-          <NavLink
-            activeClassName="active"
-            className="nav-item nav-link"
-            exact
-            to="/login"
-          >
+          <button onClick={handleLogout} className="nav-item nav-link btn">
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
